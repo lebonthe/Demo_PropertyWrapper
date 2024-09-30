7453bb87dee04e954284b08d4247f730133f77da
Min Hu <lebonthe@gmail.com>
2024-09-30 23:44:45 +0800

    -@State- Button changes a boolean value

--- Demo_PropertyWrapper/ContentView.swift
+++ Demo_PropertyWrapper/ContentView.swift
@@ -8,15 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var displayBool: Bool = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            
+            if displayBool {
+                Image(systemName: "checkmark")
+                    .foregroundStyle(.black)
+                Text("是")
+            } else {
+                Image(systemName: "x.circle")
+                    .foregroundStyle(.red)
+                Text("否")
+            }
+                
+            Button {
+                displayBool = !displayBool
+            } label: {
+                Text("切換是/否")
+            }
+            
         }
+        .font(.largeTitle)
+        .imageScale(.large)
+        .buttonBorderShape(.capsule)
+        .buttonStyle(.borderedProminent)
         .padding()
     }
+        
 }
 
 #Preview {
