ecc2b46e305e9af3515d7ccfe7893b763e3739ba
Min Hu <lebonthe@gmail.com>
2024-09-30 20:48:35 +0800

    Initial Commit

---
+++ Demo_PropertyWrapper/Demo_PropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_PropertyWrapperApp.swift
+//  Demo_PropertyWrapper
+//
+//  Created by Min Hu on 2024/9/30.
+//
+
+import SwiftUI
+
+@main
+struct Demo_PropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
